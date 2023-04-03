9555ef8af8e0833a42d6a098e799cd42356d90f3
37544 <andryxa308@>
2023-04-03 14:20:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+
+
+
+public class Main {
+    private static boolean name;
+    private static Object area;
+    private static Object perimeter;
+
+    public static void main(String[] args) {
+        Figure[] figure = {
+
+                new Circle(12.0),
+                new Rectangle(2.0, 4.0),
+                new Square(5.0),
+                new Triangle(3.0, 4.0, 5.0),
+                new Parallelogram(2.0, 3.0, 133.0)};
+        for (Figure fig : figure)
+            System.out.println(fig.getName() + ": Площадь = " + fig.getArea());
+        for (Figure fig : figure)
+        System.out.println(fig.getName() + ": Периметр = " + fig.getPerimeter());
+    }
+}
+
+
+
+
+        
+
+
+
+
+
